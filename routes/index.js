4cbc4d92d7bce3e59535d2abf85b1d929ddb8919
Luis Espinoza <espinozal@users.noreply.github.com>
2015-07-28 22:01:39 +0200

    editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,13 @@ router.get('/', function(req, res) {
 
 // autoload de comandos con :quizId
 router.param('quizId', quizController.load);
-
+// definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', quizController.author);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
-
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 module.exports = router;
